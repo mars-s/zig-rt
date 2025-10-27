8f222bf96b9b9ab4cb1e706bb491bbcaac77d3b0
Avinaba Das <71009876+mars-s@users.noreply.github.com>
2025-10-27 23:00:25 +1100

    cleaned project

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,13 @@
 const std = @import("std");
 const rt_zig = @import("rt_zig");
 
-pub fn main() !void {}
+pub fn main() !void {
+    //int
+    const image_width = 256;
+    const image_height = 256;
+
+    //renderer
+    for (0..image_height) |h| {
+        for (0..image_width) |w| {}
+    }
+}
